fb076df9801f3585107778c7919ba2fb93e498f6
Vipul Goyal <vipulkg30@gmail.com>
2021-05-23 11:29:17 +0530

    Track Order

--- src/containers/CheckoutPage/index.js
+++ src/containers/CheckoutPage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {addOrder, getAddress, getCartItems } from "../../actions";
+import { addOrder, getAddress, getCartItems } from "../../actions";
 import Layout from "../../components/Layout";
 import {
   Anchor,
@@ -104,7 +104,7 @@ const CheckoutPage = (props) => {
   const [selectedAddress, setSelectedAddress] = useState(null);
   const [orderSummary, setOrderSummary] = useState(false);
   const [orderConfirmation, setOrderConfirmation] = useState(false);
-  const [paymentoption, setPaymentOption] = useState(false);
+  const [paymentOption, setPaymentOption] = useState(false);
   const [confirmOrder, setConfirmOrder] = useState(false);
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
@@ -142,29 +142,33 @@ const CheckoutPage = (props) => {
     setOrderConfirmation(true);
     setOrderSummary(false);
     setPaymentOption(true);
-  }
+  };
 
-  const onCofirmOrder = () => {
-    const totalAmount = Object.keys(cart.cartItems).reduce((totalPrice, key) => {
+  const onConfirmOrder = () => {
+    const totalAmount = Object.keys(cart.cartItems).reduce(
+      (totalPrice, key) => {
         const { price, qty } = cart.cartItems[key];
         return totalPrice + price * qty;
-        }, 0);
-    const items = Object.keys(cart.cartItems).map(key => ({
-        productId: key, 
-        payablePrice: cart.cartItems[key].price,
-        purchasedQty: cart.cartItems[key].qty
-      }));
-
+      },
+      0
+    );
+    const items = Object.keys(cart.cartItems).map((key) => ({
+      productId: key,
+      payablePrice: cart.cartItems[key].price,
+      purchasedQty: cart.cartItems[key].qty,
+    }));
     const payload = {
-        addressId : selectedAddress._id,
-        totalAmount,
-        items,
-        paymentStatus: "pending"
+      addressId: selectedAddress._id,
+      totalAmount,
+      items,
+      paymentStatus: "pending",
+      paymentType: "cod",
     };
+
     console.log(payload);
     dispatch(addOrder(payload));
     setConfirmOrder(true);
-  }
+  };
 
   useEffect(() => {
     auth.authenticate && dispatch(getAddress());
@@ -181,11 +185,11 @@ const CheckoutPage = (props) => {
     //user.address.length === 0 && setNewAddress(true);
   }, [user.address]);
 
-  if(confirmOrder){
+  if (confirmOrder) {
     return (
       <Layout>
         <Card>
-          <div>Thank You</div>
+          <div>Thank you</div>
         </Card>
       </Layout>
     );
@@ -252,47 +256,72 @@ const CheckoutPage = (props) => {
             stepNumber={"3"}
             title={"ORDER SUMMARY"}
             active={orderSummary}
-            body={orderSummary ? (
-              <CartPage onlyCartItems={true} /> 
-              ) : orderConfirmation ? ( 
-                <div className="stepCompleted">{Object.keys(cart.cartItems).length} items</div> 
-              ) : null 
+            body={
+              orderSummary ? (
+                <CartPage onlyCartItems={true} />
+              ) : orderConfirmation ? (
+                <div className="stepCompleted">
+                  {Object.keys(cart.cartItems).length} items
+                </div>
+              ) : null
             }
           />
-          {
-            orderSummary && (
-                <Card style={{margin:"10px 0"}}>
-                <div className="flexRow sb" style={{padding: "20px", alignItems: 'center'}}>
-                  <p style={{fontSize: "12"}}>Order confirmation mail will be sent to <strong>{auth.user.email}</strong></p>
-                  <MaterialButton 
-                    title="CONTINUE" 
-                    onClick = {userOrderConfirmation}
-                    style={{width: "200px"}}/>
-                </div>
-              </Card>)
-          }
 
-          <CheckoutStep 
-            stepNumber={"4"} 
-            title={"PAYMENT OPTIONS"} 
-            active={paymentoption}
-            body=
-            {
-              paymentoption && 
+          {orderSummary && (
+            <Card
+              style={{
+                margin: "10px 0",
+              }}
+            >
+              <div
+                className="flexRow sb"
+                style={{
+                  padding: "20px",
+                  alignItems: "center",
+                }}
+              >
+                <p style={{ fontSize: "12px" }}>
+                  Order confirmation email will be sent to{" "}
+                  <strong>{auth.user.email}</strong>
+                </p>
+                <MaterialButton
+                  title="CONTINUE"
+                  onClick={userOrderConfirmation}
+                  style={{
+                    width: "200px",
+                  }}
+                />
+              </div>
+            </Card>
+          )}
+
+          <CheckoutStep
+            stepNumber={"4"}
+            title={"PAYMENT OPTIONS"}
+            active={paymentOption}
+            body={
+              paymentOption && (
                 <div>
-                  <div className="flexRow" style={{ alignItems: 'center', padding: "20px"}}>
+                  <div
+                    className="flexRow"
+                    style={{
+                      alignItems: "center",
+                      padding: "20px",
+                    }}
+                  >
                     <input type="radio" name="paymentOption" value="cod" />
-                    <div>Cash On Delivery</div>
+                    <div>Cash on delivery</div>
                   </div>
-                  <MaterialButton 
+                  <MaterialButton
                     title="CONFIRM ORDER"
-                    onClick={onCofirmOrder}
+                    onClick={onConfirmOrder}
                     style={{
                       width: "200px",
-                      margin: "0 0 20px 20px"
+                      margin: "0 0 20px 20px",
                     }}
                   />
                 </div>
+              )
             }
           />
         </div>

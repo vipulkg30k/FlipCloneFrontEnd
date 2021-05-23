4e3e2bcb8de0afeba72bde3eac77691706015a89
Vipul Goyal <vipulkg30@gmail.com>
2021-05-23 10:50:22 +0530

    jwt expires fix

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { api } from '../uriConfig';
 import store from '../store';
+import { authConstants } from '../actions/constants';
 
 const token = window.localStorage.getItem('token');
 
@@ -19,4 +20,17 @@ axiosIntance.interceptors.request.use((req) => {
     return req;
 })
 
+axiosIntance.interceptors.response.use((res) => {
+    return res;
+}, (error) => {
+    console.log(error.response);
+    const status = error.response ? error.response.status : 500;
+    if(status && status === 500){
+        localStorage.clear();
+        store.dispatch({ type: authConstants.LOGOUT_SUCCESS });
+    }
+    return Promise.reject(error);
+});
+
 export default axiosIntance;
+            

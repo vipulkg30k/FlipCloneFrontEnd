106151a618fe4d2de30638f9d47919b742043b6a
Vipul Goyal <vipulkg30@gmail.com>
2021-05-29 10:57:44 +0530

    Some Bugs fix and Order Tracker added

--- src/components/MaterialUI/index.js
+++ src/components/MaterialUI/index.js
@@ -108,30 +108,26 @@ const DropdownMenu = (props) => {
     <div className="headerDropdownContainer">
       {props.menu}
       <div className="dropdown">
-        <div className="upArrowContainer">
-          <div className="upArrow"></div>
-        </div>
-        <div className="dropdownMenu">
-          {props.firstMenu}
-          <ul className="headerDropdownMenu">
-            {props.menus &&
-              props.menus.map((item, index) => (
-                <li key={index}>
-                  <a
-                    onClick={(e) => {
-                      if (item.onClick) {
-                        e.preventDefault();
-                        item.onClick && item.onClick();
-                      }
-                    }}
-                    href={`${item.href}`}
-                  >
-                    {item.label}
-                  </a>
-                </li>
-              ))}
-          </ul>
-        </div>
+        <div className="upArrow"></div>
+        {props.firstMenu}
+        <ul className="headerDropdownMenu">
+          {props.menus &&
+            props.menus.map((item, index) => (
+              <li key={index}>
+                <a
+                  onClick={(e) => {
+                    if (item.onClick) {
+                      e.preventDefault();
+                      item.onClick && item.onClick();
+                    }
+                  }}
+                  href={`${item.href}`}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+        </ul>
       </div>
     </div>
   );
